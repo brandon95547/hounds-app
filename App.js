6b5fdd1df81fd87777ec5c4bee80797ae98baff8
Brandon Sanders <brandon95547@gmail.com>
2020-05-12 17:08:08 -0400

    removing unneccesary imports

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import { View, Text, Button } from 'react-native'
 // navigation
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
